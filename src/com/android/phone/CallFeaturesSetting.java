99dfcfb91ba30629b40da35e3880e1f3e11713a3
mengsun <msun@codeaurora.org>
2013-12-11 17:56:42 +0800

    TeleService: Add a controller for in-call proximity sensor (3/3)
    * Add the setting for enable proximity sensor or not when calling
    
    Change-Id: I42bc05ae1c22443cca782ac1ae66d5d5bb4ac858
    Signed-off-by: Janson Kang <temasek71@gmail.com>
    Signed-off-by: Varun <date.varun123@gmail.com>

--- src/com/android/phone/CallFeaturesSetting.java
+++ src/com/android/phone/CallFeaturesSetting.java
@@ -109,6 +109,7 @@ public class CallFeaturesSetting extends PreferenceActivity
     private static final String ENABLE_VIDEO_CALLING_KEY = "button_enable_video_calling";
 
     private static final String FLIP_ACTION_KEY = "flip_action";
+    private static final String BUTTON_PROXIMITY_KEY   = "button_proximity_key";
 
     private Phone mPhone;
     private SubscriptionInfoHelper mSubscriptionInfoHelper;
@@ -121,6 +122,7 @@ public class CallFeaturesSetting extends PreferenceActivity
     private PreferenceScreen mVoicemailSettingsScreen;
     private SwitchPreference mEnableVideoCalling;
     private PreferenceScreen mButtonBlacklist;
+    private SwitchPreference mButtonProximity;
 
     private ListPreference mFlipAction;
 
@@ -135,6 +137,16 @@ public class CallFeaturesSetting extends PreferenceActivity
             android.provider.Settings.Global.putInt(mPhone.getContext().getContentResolver(),
                     android.provider.Settings.Global.CALL_AUTO_RETRY,
                     mButtonAutoRetry.isChecked() ? 1 : 0);
+            return true;
+        } else if (preference == mButtonProximity) {
+            int checked = mButtonProximity.isChecked() ? 1 : 0;
+            Settings.System.putInt(mPhone.getContext().getContentResolver(),
+                    Settings.System.IN_CALL_PROXIMITY_SENSOR, checked);
+            if (checked == 1) {
+                mButtonProximity.setSummary(R.string.proximity_on_summary);
+            } else {
+                mButtonProximity.setSummary(R.string.proximity_off_summary);
+            }
             return true;
         }
         return false;
@@ -197,6 +209,8 @@ public class CallFeaturesSetting extends PreferenceActivity
         super.onCreate(icicle);
         if (DBG) log("onCreate: Intent is " + getIntent());
 
+        PreferenceScreen preferenceScreen = getPreferenceScreen();
+
         // Make sure we are running as the primary user.
         if (UserHandle.myUserId() != UserHandle.USER_OWNER) {
             Toast.makeText(this, R.string.call_settings_primary_user_only,
@@ -210,6 +224,15 @@ public class CallFeaturesSetting extends PreferenceActivity
                 getActionBar(), getResources(), R.string.call_settings_with_label);
         mPhone = mSubscriptionInfoHelper.getPhone();
         mTelecomManager = TelecomManager.from(this);
+
+        if (mButtonProximity != null) {
+            if (getResources().getBoolean(R.bool.config_proximity_enable)) {
+                mButtonProximity.setOnPreferenceChangeListener(this);
+            } else {
+                getPreferenceScreen().removePreference(mButtonProximity);
+                mButtonProximity = null;
+            }
+        }
     }
 
     @Override
@@ -255,6 +278,8 @@ public class CallFeaturesSetting extends PreferenceActivity
             prefSet.removePreference(mButtonAutoRetry);
             mButtonAutoRetry = null;
         }
+
+        mButtonProximity = (SwitchPreference) findPreference(BUTTON_PROXIMITY_KEY);
 
         Preference cdmaOptions = prefSet.findPreference(BUTTON_CDMA_OPTIONS);
         Preference gsmOptions = prefSet.findPreference(BUTTON_GSM_UMTS_OPTIONS);
@@ -363,6 +388,13 @@ public class CallFeaturesSetting extends PreferenceActivity
             updateFlipActionSummary(flipAction);
         }
 
+        if (mButtonProximity != null) {
+            boolean checked = Settings.System.getInt(getContentResolver(),
+                    Settings.System.IN_CALL_PROXIMITY_SENSOR, 1) == 1;
+            mButtonProximity.setChecked(checked);
+            mButtonProximity.setSummary(checked ? R.string.proximity_on_summary
+                    : R.string.proximity_off_summary);
+        }
     }
 
     public static boolean isPackageInstalled(Context context, String pkg, boolean ignoreState) {
